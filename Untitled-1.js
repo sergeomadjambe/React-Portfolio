81e8335cacc6872d1ab76720c9ac527602641052
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2023-04-15 10:47:57 -0400

    set up my portfolio

---
+++ Untitled-1.js
@@ -0,0 +1,224 @@
+
+
+
+export default function Portfolio() {
+    return (
+        <div>
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a href="https://github.com/sergeomadjambe/team-profile-generator">
+  
+    <div className="card bg-dark text-white" id="work">
+      <img  src= " " alt='password generator' class="card-img-top" ></img>
+      
+        <h5 className="card-title">Team Profile Generator</h5>
+      
+    </div>
+     
+    </a >
+    <a className='repo' href='https://github.com/sergeomadjambe/team-profile-generator'>[repo]</a>
+</div>
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="https://github.com/sergeomadjambe/pwa-text-editor">
+    <div className="card bg-dark text-white" id="work">
+        <img src= "" class="card-img-top" alt="daily planner"></img>
+       
+          <h5 className="card-title">Daily Planner</h5>
+     </div>
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/pwa-text-editor'>[repo]</a>
+
+</div>
+
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="https://github.com/sergeomadjambe/horiseon-marketing">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="coding quiz"></img>
+
+          <h5 className="card-title">Coding Quiz</h5> 
+          </div>
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/horiseon-marketing'>[repo]</a>
+
+</div>
+
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="https://calm-lowlands-90253.herokuapp.com/">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="coding quiz"></img>
+
+          <h5 className="card-title">Text Editor</h5> 
+          </div>
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/coding-quiz'>[repo]</a>
+
+</div>
+
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a   href="https://github.com/sergeomadjambe/Employee-Tracker">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="weather board"></img>
+
+          <h5 className="card-title">Weather Dashboard</h5>
+         </div>
+
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/Employee-Tracker'>[repo]</a>
+
+</div>
+
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="https://github.com/sergeomadjambe/README-Generator">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="happy hour log"></img>
+
+          <h5 className="card-title">Happy Hour Log</h5>
+         </div>
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/README-Generator'>[repo]</a>
+
+</div>
+
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a   href="https://github.com/sergeomadjambe/password-generator">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="bucket list"></img>
+
+          <h5 className="card-title">Bucket list</h5>
+         </div>
+
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/password-generator'>[repo]</a>
+
+</div>
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a   href="https://github.com/sergeomadjambe/Social-Network-api">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="bucket list"></img>
+
+          <h5 className="card-title">Social Network</h5>
+         </div>
+
+  </a>
+  <a className='repo' href='https://github.com/sergeomadjambe/Social-Network-api'>[repo]</a>
+
+</div>
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="team board"></img>
+
+         
+           <h5 className="card-title">Team Board [repo]</h5>
+          </div>
+   </a>
+   <p></p>
+</div>
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a href="https://github.com/sergeomadjambe/regex-tutoria">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="ecommerce backend"></img>
+
+          <h5 className="card-title">e-commerce Back End [repo]</h5>
+        </div>
+  </a>
+  <p></p>
+
+</div>
+
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="https://github.com/sergeomadjambe/daily-planner">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="note taker"></img>
+
+          <h5 className="card-title">Note Taker [repo]</h5>
+        </div>  
+  </a>
+  <p></p>
+
+</div>
+
+<div className="paper">
+  <div className="pin">
+    <div className="shadow"></div>
+    <div className="metal"></div>
+    <div className="bottom-circle"></div>
+  </div>
+  <a  href="https://github.com/sergeomadjambe/daily-planner">
+    <div className="card bg-dark text-white" id="work">
+    <img src= " " class="card-img-top" alt="employee tracker"></img>
+
+          <h5 className="card-title">Employee Tracker [repo]</h5>
+        </div>  
+  </a>
+  <p></p>
+
+</div>
+        </div>
+    )
+}
